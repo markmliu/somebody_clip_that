6add4a08e3a50f5b13860604db5c8aabbcd14cec
Jordan Liu <jordanminjie@gmail.com>
2021-07-02 18:31:09 +0000

    Hardcode REDIRECT_URL for now
    There seems to be a problem with using request.path.split('?')[0]

--- app.py
+++ app.py
@@ -10,26 +10,20 @@ CLIENT_ID = "iliayvc79au07dbm3hk21a0f1y730k"
 #TODO this url needs to be the server's URL, instead of hardcoded as localhost
 REDIRECT_URL = "https://somebodyclipthat-nekfxpjbea-uw.a.run.app/login"
 SCOPE = "clips:edit user:read:email"
+CLIENT_SECRET = ""
 
 def get_login_link():
-    # read client secret from env variable
-    global CLIENT_SECRET
-    CLIENT_SECRET = os.environ["SOMEBODY_CLIP_THAT_CLIENT_SECRET"]
-    print ("client secret: ", CLIENT_SECRET)
-
-    login_url = "https://id.twitch.tv/oauth2/authorize?client_id="+CLIENT_ID+"&redirect_uri="+REDIRECT_URL+"&response_type=code&scope="+SCOPE
-    return login_url
-
+    return "https://id.twitch.tv/oauth2/authorize?client_id="+CLIENT_ID+"&redirect_uri="+REDIRECT_URL+"&response_type=code&scope="+SCOPE
 
 app = Flask(__name__, template_folder="templates")
 
-def get_access_token(code, redirect_url):
+def get_access_token(code):
     url = "https://id.twitch.tv/oauth2/token"
     params = {"client_id":CLIENT_ID,
               "client_secret":CLIENT_SECRET,
               "code":code,
               "grant_type":"authorization_code",
-              "redirect_uri":redirect_url}
+              "redirect_uri":REDIRECT_URL}
     x = requests.post(url,params=params)
 
     print(x.json())
@@ -40,6 +34,7 @@ def get_access_token(code, redirect_url):
 @app.route("/")
 def index():
     access_token = request.cookies.get('access_token')
+    print("url=", request.url.split('?')[0])
     if access_token is None:
         return render_template('index.html', logged_in=False, login_link=get_login_link())
     else:
@@ -52,7 +47,7 @@ def login():
     print("code in url params:", code)
     print("url:", url)
 
-    access_token = get_access_token(code, url)
+    access_token = get_access_token(code)
     if access_token:
         resp = make_response(render_template('index.html', logged_in=True, client_id=CLIENT_ID))
         resp.set_cookie('access_token', access_token)
@@ -63,11 +58,10 @@ def login():
 
 if __name__ == "__main__":
     # read client secret from env variable
-    global CLIENT_SECRET
     CLIENT_SECRET = os.environ["SOMEBODY_CLIP_THAT_CLIENT_SECRET"]
     print("client secret: ", CLIENT_SECRET)
 
     app.jinja_env.auto_reload = True
     app.config['TEMPLATES_AUTO_RELOAD'] = True
     server_port = os.environ.get('PORT', '8080')
-    app.run(host='0.0.0.0', port=server_port)
+    app.run(debug=True, host='0.0.0.0', port=server_port)
