52dc38cd12e5a0849f5e6da564a3da193e594cdd
Jordan Liu <jordanminjie@gmail.com>
2021-07-02 17:06:54 +0000

    Add dockerfile

--- app.py
+++ app.py
@@ -7,21 +7,8 @@ import json
 import requests
 
 CLIENT_ID = "iliayvc79au07dbm3hk21a0f1y730k"
-#TODO this url needs to be the server's URL, instead of hardcoded as localhost
-REDIRECT_URL = "http://localhost:5000/login"
-CLIENT_SECRET = ""
-SCOPE = "clips:edit user:read:email"
 
 app = Flask(__name__, template_folder="templates")
-
-def get_login_link():
-  # read client secret from env variable
-  global CLIENT_SECRET
-  CLIENT_SECRET = os.environ["SOMEBODY_CLIP_THAT_CLIENT_SECRET"]
-  print ("client secret: ", CLIENT_SECRET)
-
-  login_url = "https://id.twitch.tv/oauth2/authorize?client_id="+CLIENT_ID+"&redirect_uri="+REDIRECT_URL+"&response_type=code&scope="+SCOPE
-  return login_url
 
 def get_access_token(code):
     url = "https://id.twitch.tv/oauth2/token"
@@ -41,7 +28,7 @@ def get_access_token(code):
 def index():
     access_token = request.cookies.get('access_token')
     if access_token is None:
-        return render_template('index.html', logged_in=False, login_link=get_login_link())
+        return render_template('index.html', logged_in=False
     else:
         return render_template('index.html', logged_in=True, client_id=CLIENT_ID)
 
@@ -55,6 +42,12 @@ def login():
     return resp
 
 if __name__ == "__main__":
+    # read client secret from env variable
+    global CLIENT_SECRET
+    CLIENT_SECRET = os.environ["SOMEBODY_CLIP_THAT_CLIENT_SECRET"]
+    print("client secret: ", CLIENT_SECRET)
+
     app.jinja_env.auto_reload = True
     app.config['TEMPLATES_AUTO_RELOAD'] = True
-    app.run(debug=True, host='0.0.0.0')
+    server_port = os.environ.get('PORT', '8080')
+    app.run(host='0.0.0.0', port=server_port)
